335a44f664a864ec741dc7fe5ce36e60ea1db02d
Abir Khan <ak99.abirkhan@gmail.com>
2024-11-29 22:20:36 +0600

    update

--- backend/index.js
+++ backend/index.js
@@ -5,20 +5,29 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 
 import dotenv from "dotenv";
+import path from "path";
 
 dotenv.config();
 
 
 const app = express();
 const port = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
-app.use(cors({origin: "http://localhost:5173", credentials: true}));
+app.use(cors({origin: "*", credentials: true}));
 
 app.use(express.json()); // for parsing application/json
 
 app.use(cookieParser()); // allow us to parse incoming cookies
 
 app.use("/api/auth", authRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "frontend/build")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+    });
+}
 
 app.listen(port, () => {
     connectDB();
